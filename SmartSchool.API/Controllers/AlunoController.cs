17abe6dc3774479e8b6c521dfceb68478074349d
Thiago <thiagoqueiroz97@gmail.com>
2022-06-25 15:59:08 -0300

    first commit

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno() {
+                Id = 1,
+                Nome = "Marcos",
+                Sobrenome = "Almeida",
+                Telefone = "123456123"
+            },
+            new Aluno() {
+                Id = 2,
+                Nome = "Marta",
+                Sobrenome = "Kente",
+                Telefone = "12312314"
+            },
+            new Aluno() {
+                Id = 3,
+                Nome = "Laura",
+                Sobrenome = "Maria",
+                Telefone = "4567463"
+            },
+        };
+
+        public AlunoController() { }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        [HttpGet("byId/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("O aluno nao foi encontrado");
+            return Ok(aluno);
+        }
+
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome, string Sobrenome)
+        {
+            var aluno = Alunos.FirstOrDefault(a =>
+            a.Nome.Contains(nome) && a.Sobrenome.Contains(Sobrenome)
+            );
+            if (aluno == null) return BadRequest("O aluno nao foi encontrado");
+            return Ok(aluno);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ SmartSchool.API/Models/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace SmartSchool.API.Models
+{
+    public class Aluno
+    {
+        public Aluno() { }
+
+        public Aluno(string telefone, string nome, string sobrenome, int id)
+        {
+            this.Telefone = telefone;
+            this.Nome = nome;
+            this.Sobrenome = sobrenome;
+            this.Id = id;
+        }
+
+        public string Telefone { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Id { get; set; }
+
+        public IEnumerable<AlunoDiscipline> AlunosDisciplinas { get; set; }
+
+    }
+}
